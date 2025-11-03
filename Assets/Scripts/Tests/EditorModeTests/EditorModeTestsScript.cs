66ca8a69e58ad8cb7d9ae4a7268ef88ab3708590
Bobi <bobymanti@gmail.com>
2025-11-03 11:02:18 +0100

    Added simple test for MVVM, escape trigger in VM and load and save of M

--- Assets/Scripts/Tests/EditorModeTests/EditorModeTestsScript.cs
+++ Assets/Scripts/Tests/EditorModeTests/EditorModeTestsScript.cs
@@ -1,20 +1,73 @@
-using System.Collections;
 using NUnit.Framework;
-using UnityEngine.TestTools;
+using UI;
 
 namespace EditorModeTests
 {
-    public class EditorModeTestsScript
+    public class ModelTests
     {
-        [Test]
-        public void PlayModeTestScriptSimplePasses()
+        private class TestModel : Model
         {
+            public int Data { get; private set; }
+            public bool LoadCalled { get; private set; }
+            public bool SaveCalled { get; private set; }
+
+            public override void Load()
+            {
+                LoadCalled = true;
+                Data = 42;
+            }
+
+            public override void Save()
+            {
+                SaveCalled = true;
+            }
         }
 
-        [UnityTest]
-        public IEnumerator PlayModeTestScriptWithEnumeratorPasses()
+        [Test]
+        public void Model_Load_SetsData()
         {
-            yield return null;
+            var model = new TestModel();
+            model.Load();
+
+            Assert.IsTrue(model.LoadCalled);
+            Assert.AreEqual(42, model.Data);
+        }
+
+        [Test]
+        public void Model_Save_IsCalled()
+        {
+            var model = new TestModel();
+            model.Save();
+
+            Assert.IsTrue(model.SaveCalled);
+        }
+    }
+
+    public class ViewModelLogicTests
+    {
+        private class TestViewModel : ViewModel
+        {
+            public bool EscapeTriggered { get; private set; }
+
+            protected override void OnEscapeTriggered()
+            {
+                EscapeTriggered = true;
+            }
+
+            // Expose for testing
+            public void TriggerEscape() => OnEscapeTriggered();
+        }
+
+        [Test]
+        public void ViewModel_EscapeCallback_CanBeTriggered()
+        {
+            var vm = new TestViewModel();
+
+            Assert.IsFalse(vm.EscapeTriggered);
+
+            vm.TriggerEscape();
+
+            Assert.IsTrue(vm.EscapeTriggered);
         }
     }
 }
